e788ecfc29d01a4570c8ad3bb38d6e7514501dc4
Tinara Nathania <tinaranathania@gmail.com>
2022-04-27 16:30:57 +0700

    Initial Commit

---
+++ GenshinDatabaseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GenshinDatabaseApp
+//
+//  Created by Tinara Nathania Wiryonoputro on 27/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GenshinDatabaseApp/GenshinDatabaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenshinDatabaseAppApp.swift
+//  GenshinDatabaseApp
+//
+//  Created by Tinara Nathania Wiryonoputro on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct GenshinDatabaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
